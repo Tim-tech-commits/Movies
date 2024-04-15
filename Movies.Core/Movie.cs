dbddc31699ed949a052605df62429290e436a8df
Timothy Debono <debonotimothy@gmail.com>
2024-04-16 00:06:41 +0200

    Add project files.

---
+++ Movies.Core/Movie.cs
@@ -0,0 +1,25 @@
+﻿namespace Movies.Core
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Genre { get; set; }
+
+        public string Director { get; set; }
+
+        public List<string> Actors { get; set; }
+
+        public double Rating { get; set; }
+
+        public Movie()
+        {
+            Genre = [];
+            Actors = [];
+            Title = string.Empty;
+            Director = string.Empty;
+        }
+    }
+}
